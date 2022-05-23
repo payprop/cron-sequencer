payprop/cron-sequencer

commit 292f296d7fd91798e4b4582fff5a80d908f84d66

Add a --no-group option, that disables the default event grouping

lib/Cron/Sequencer/Output.pm
@@ -5,7 +5,7 @@ use warnings;
 
 package Cron::Sequencer::Output;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp qw(confess croak);
 
@@ -15,16 +15,18 @@ sub new {
     confess('new() called as an instance method')
         if ref $class;
 
-    my %state = %opts{count};
-    if ($opts{'hide-env'}) {
-        ++$state{hide_env};
-    }
+    my %state = %opts{qw(count group hide-env)};
 
     return bless \%state;
 }
 
 sub render {
     my ($self, @groups) = @_;
+
+    # We assume that you normally want things grouped, so we aren't particularly
+    # optimising the "flat" --no-group path:
+    @groups = map { [$_] } map { @$_ } @groups
+        unless $self->{group};
 
     my @output;
 

bin/cron-sequencer
@@ -100,6 +100,13 @@ Hence C<--from +0 --to +3600> shows events from midnight until 00:59 inclusive.
 
 Don't show environment variables in the output.
 
+=item --no-group
+
+Don't group events that happen at the same time. By default, the timestamp is
+shown once and then all events firing at that time are grouped together, to make
+it clearer that they are run simultaneously. With the C<--no-group> option,
+each event is treated independently and shown with its own timestamp.
+
 =item --help
 
 Shows this documentation

lib/Cron/Sequencer/CLI.pm
@@ -9,7 +9,7 @@ use parent qw(Exporter);
 require DateTime;
 use Getopt::Long qw(GetOptionsFromArray);
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 our @EXPORT_OK = qw(calculate_start_end parse_argv);
 
 sub parse_argv {
@@ -29,7 +29,9 @@ sub parse_argv {
     }
     push @groups, $current;
 
-    my %global_options;
+    my %global_options = (
+        group => 1,
+    );
 
     Getopt::Long::Configure('pass_through', 'auto_version', 'auto_help');
     unless(GetOptionsFromArray($groups[0], \%global_options,
@@ -37,6 +39,7 @@ sub parse_argv {
                                'from=s',
                                'to=s',
                                'hide-env',
+                               'group!',
                            )) {
         $pod2usage->(exitval => 255, verbose => 1);
     }
@@ -64,7 +67,7 @@ sub parse_argv {
     $pod2usage->(exitval => 255)
         unless @input;
 
-    my $output = [%global_options{qw(hide-env)}, count => scalar @input];
+    my $output = [%global_options{qw(hide-env group)}, count => scalar @input];
     return ($start, $end, $output, @input);
 }
 

t/cli-parse_argv.t
@@ -88,10 +88,10 @@ for my $flat ('--help', '--help --', '-- --help', '--version', '--version --') {
     cmp_deeply(\@warnings, [], "no warnings for $flat");
 }
 
-my $default_output = ['hide-env', undef, count => 1];
+my $default_output = ['hide-env', undef, group => 1, count => 1, ];
 my $default_for_file = {env => undef, ignore => undef, source => "file"};
 my @defaults = (@today, $default_output);
-my @defaults2 = (@today, ['hide-env', undef, count => 2]);
+my @defaults2 = (@today, ['hide-env', undef, group =>1, count => 2]);
 
 for (["file", [@defaults, $default_for_file]],
      ["file --show today", [@defaults, $default_for_file]],
@@ -101,7 +101,8 @@ for (["file", [@defaults, $default_for_file]],
      ["--from 1 --to 11 -- file", [1, 11, $default_output, $default_for_file]],
      ["--from 1 --to 11 file --", [1, 11, $default_output, $default_for_file]],
 
-     ["--hide-env file", [@today, ['hide-env', 1, 'count', 1], $default_for_file]],
+     ["--hide-env file", [@today, ['hide-env', 1, group => 1, count => 1],
+                          $default_for_file]],
      ["--env=FOO=BAR file --env BAZ=",
       [@defaults, {env => ["FOO=BAR", "BAZ="], ignore => undef, source => "file"}]],
      ["-- --env=FOO=BAR file --env BAZ=",
@@ -135,7 +136,7 @@ for (["file", [@defaults, $default_for_file]],
        {env => ["BAZ="], ignore => undef, source => "file2"},
    ]],
      ["file1 --env=FOO=BAR file2 -- file3 file4 -- file5 --env BAZ= file6",
-      [@today, ['hide-env', undef, count => 6],
+      [@today, ['hide-env', undef, group => 1, count => 6],
        {env => ["FOO=BAR"], ignore => undef, source => "file1"},
        {env => ["FOO=BAR"], ignore => undef, source => "file2"},
        {env => undef, ignore => undef, source => "file3"},

commit 24fb7ff8e8b06e228febb8a6382857c7ecbdd6c9

Fix --hide-env in the default (text) output
This is a regression introduced by commit 292f296d7fd91798:
    Add a --no-group option, that disables the default event grouping

Regression testing the output remains a TODO, and this bug shows that the
current interim solution of local "golden results" is fragile.

Not addressing testing *yet* - prioritising getting this fix shipped, rather
than delaying shipping the fix until I have time to decide on how and how much
to test the output.

lib/Cron/Sequencer.pm
@@ -5,7 +5,7 @@ use warnings;
 
 package Cron::Sequencer;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Carp qw(croak confess);
 

lib/Cron/Sequencer/Output.pm
@@ -9,7 +9,7 @@ no warnings 'experimental::postderef';
 
 package Cron::Sequencer::Output;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Carp qw(confess croak);
 
@@ -60,8 +60,7 @@ sub render_text {
                 push @cluster, "line $entry->{lineno}: $entry->{when}";
             }
 
-            unless ($self->{hide_env}) {
-                local *_;
+            unless ($self->{'hide-env'}) {
                 push @cluster, map "unset $_", $entry->{unset}->@*
                     if $entry->{unset};
                 my $env = $entry->{env};